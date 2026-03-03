23fa3d131101551898fd83a516ff978d0a3ba4ed
soportedepowerluki-cell <soportedepowerluki@gmail.com>
2026-03-03 12:24:05 -0600

    index.js

--- src/index.js
+++ src/index.js
@@ -1,6 +1,4 @@
-if (process.env.NODE_ENV !== 'production') {
-  require('dotenv').config();
-}
+require('dotenv').config();
 const {
     Client,
     GatewayIntentBits,
@@ -332,4 +330,5 @@ client.login(process.env.DISCORD_TOKEN)
 
     process.exit(1);
   });
+
 
