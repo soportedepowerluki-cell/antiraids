81b7a50fca049a56b4b9f7933c4a3ac23282a100
soportedepowerluki-cell <soportedepowerluki@gmail.com>
2026-03-21 17:10:17 -0600

    Update index.js

--- index.js
+++ index.js
@@ -1,8 +1,15 @@
+const http = require('http');
 const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({
   intents: [GatewayIntentBits.Guilds]
 });
+
+// 👇 ESTO ES OBLIGATORIO EN RENDER
+http.createServer((req, res) => {
+  res.writeHead(200, { 'Content-Type': 'text/plain' });
+  res.end('Bot activo');
+}).listen(process.env.PORT || 3000);
 
 client.once('ready', () => {
   console.log(`✅ CONECTADO COMO ${client.user.tag}`);
